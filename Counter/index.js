efe92ccc0d7523c2e7625b6201307584e87fcee7
Jiyon Lee <jiyonlee.d@gmail.com>
2020-12-30 21:51:23 +0900

    using closure

--- Counter/index.js
+++ Counter/index.js
@@ -4,7 +4,7 @@ const increase = document.querySelector(".increase");
 const number = document.querySelector(".number");
 const btn = document.querySelectorAll(".btn");
 
-var changeColor = function () {
+const changeColor = function () {
   if (number.textContent < 0) {
     number.style.color = "red";
   } else if (number.textContent > 0) {
@@ -14,13 +14,37 @@ var changeColor = function () {
   }
 };
 
-var changeNum = function () {};
+function changeNum() {
+  let num = 0;
+
+  return {
+    increase() {
+      num++;
+      number.textContent = num;
+    },
+    decrease() {
+      num--;
+      number.textContent = num;
+    },
+    reset() {
+      num = 0;
+      number.textContent = num;
+    },
+  };
+}
+var num = changeNum();
 
 btn.forEach(function (button) {
   button.addEventListener("click", function () {
     if (button.textContent === "DECREASE") {
+      num.decrease();
+      changeColor();
     } else if (button.textContent === "RESET") {
+      num.reset();
+      changeColor();
     } else if (button.textContent === "INCREASE") {
+      num.increase();
+      changeColor();
     }
   });
 });
