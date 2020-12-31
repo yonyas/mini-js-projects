588c99fa9bf346f7a998a5d97a1c8f0742386509
Jiyon Lee <jiyonlee.d@gmail.com>
2020-12-31 17:55:06 +0900

    finish 20201231

--- Navbar/index.js
+++ Navbar/index.js
@@ -1,4 +1,14 @@
 const barBtn = document.querySelector(".bar-btn");
 const gnb = document.querySelector(".gnb");
 
-barBtn.addEventListener("click", function () {});
+barBtn.addEventListener("click", function () {
+  //   if (gnb.classList.contains("hidden")) {
+  //     gnb.classList.remove("hidden");
+  //     gnb.classList.add("show");
+  //   } else {
+  //     gnb.classList.remove("show");
+
+  //     gnb.classList.add("hidden");
+  //   }
+  gnb.classList.toggle("show");
+});
