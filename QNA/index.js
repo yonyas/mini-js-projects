d9c696bb9e6373bf4699f9a876de4d82be61d155
Jiyon Lee <jiyonlee.d@gmail.com>
2021-01-02 11:06:02 +0900

    hide other answer

--- QNA/index.js
+++ QNA/index.js
@@ -1,13 +1,20 @@
-const qbox = document.querySelectorAll(".qbox");
+const qboxs = document.querySelectorAll(".qbox");
 const plus = document.querySelector(".plus");
 const minus = document.querySelector(".minus");
 const answer = document.querySelector(".answer");
 
-qbox.forEach(function (n, i) {
-  n.addEventListener("click", function (e) {
-    console.log(e.currentTarget);
+qboxs.forEach(function (qbox, i) {
+  qbox.addEventListener("click", function (e) {
+    // console.log(qboxs);
+    // console.log(e.currentTarget);
+    // console.log(qboxs[0] === e.currentTarget);
     e.currentTarget.querySelector(".plus").classList.toggle("hide");
     e.currentTarget.querySelector(".minus").classList.toggle("hide");
     e.currentTarget.querySelector(".answer").classList.toggle("hide");
+    qboxs.forEach(function (item) {
+      if (item !== e.currentTarget) {
+        item.querySelector(".answer").classList.add("hide");
+      }
+    });
   });
 });
