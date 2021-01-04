eae40cade21529b549fdd614d2eb3b8693803a3f
Jiyon Lee <jiyonlee.d@gmail.com>
2021-01-04 23:07:38 +0900

    fixed navbar and toTopBtn

--- Scroll/index.js
+++ Scroll/index.js
@@ -4,9 +4,35 @@ date.textContent = new Date().getFullYear();
 
 // nav
 const toggleMenu = document.querySelector(".toggle-menu");
+const linksContainer = document.querySelector(".links-container");
 const links = document.querySelector(".links");
 
 toggleMenu.addEventListener("click", function () {
-  links.classList.toggle("show-links");
-  links.classList.toggle("hide-links");
+  const linksContainerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+
+  if (linksContainerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+});
+
+// fixed nav
+const nav = document.querySelector("nav");
+const NAV_INVISIBLE_HEIGHT = 90;
+const toTopBtn = document.querySelector(".to-top-btn");
+
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset >= NAV_INVISIBLE_HEIGHT) {
+    nav.classList.add("fixed-nav");
+  } else {
+    nav.classList.remove("fixed-nav");
+  }
+
+  if (window.pageYOffset >= 500) {
+    toTopBtn.classList.remove("hide");
+  } else {
+    toTopBtn.classList.add("hide");
+  }
 });
