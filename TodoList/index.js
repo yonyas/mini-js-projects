ed7d66eca5a165ec78dc2130948fac75a3b352a7
Jiyon Lee <jiyonlee.d@gmail.com>
2021-01-11 11:04:44 +0900

    add edit func

--- TodoList/index.js
+++ TodoList/index.js
@@ -1,6 +1,9 @@
 const submitBtn = document.querySelector(".submit");
 const todoInput = document.querySelector(".todo-input");
 const lists = document.querySelector(".lists");
+let editBtn = document.querySelectorAll(".edit-btn");
+let deleteBtn = document.querySelectorAll(".delete-btn");
+let stopflag = false;
 
 let inputList = [];
 
@@ -21,25 +24,62 @@ const arrayToDom = function (array) {
 function deleteList(deleteBtn, inputList) {
   deleteBtn.forEach(function (item, i) {
     item.addEventListener("click", (e) => {
+      // delete 누르면 inputList에서 해당 인덱스의 값 삭제 후 다시 그리기
       inputList.splice(i, 1);
       arrayToDom(inputList);
 
+      editBtn = document.querySelectorAll(".edit-btn");
       deleteBtn = document.querySelectorAll(".delete-btn");
       deleteList(deleteBtn, inputList);
+      editList(editBtn, inputList);
+    });
+  });
+}
+function editList(editBtn, inputList) {
+  // edit누르면 inputList의 해당 인덱스를 input 에서 수정하게 하고, DOM 다시 그리기
+  editBtn.forEach(function (item, i) {
+    item.addEventListener("click", function (e) {
+      stopflag = true;
+      todoInput.value = inputList[i];
+      todoInput.focus();
 
-      console.log(inputList);
-      console.log(deleteBtn);
+      //edit 클래스 추가해서 기능 구현
+      submitBtn.textContent = "Edit";
+      submitBtn.classList.add("edit");
+      let editSubmitBtn = document.querySelector(".edit");
+
+      editSubmitBtn.addEventListener("click", () => {
+        todoInput.focus();
+        inputList[i] = todoInput.value;
+        arrayToDom(inputList);
+
+        // 원상복귀
+        i = "";
+        editBtn = document.querySelectorAll(".edit-btn");
+        deleteBtn = document.querySelectorAll(".delete-btn");
+        editSubmitBtn.textContent = "Submit";
+        editSubmitBtn.classList.remove("edit");
+        stopflag = false;
+        deleteList(deleteBtn, inputList);
+        editList(editBtn, inputList);
+      });
     });
   });
 }
 
 submitBtn.addEventListener("click", (e) => {
+  if (stopflag === true) return;
   e.preventDefault();
   inputList.push(todoInput.value);
   arrayToDom(inputList);
+  todoInput.focus();
+  todoInput.value = "";
 
-  let editBtn = document.querySelectorAll(".edit-btn");
-  let deleteBtn = document.querySelectorAll(".delete-btn");
+  editBtn = document.querySelectorAll(".edit-btn");
+  deleteBtn = document.querySelectorAll(".delete-btn");
 
   deleteList(deleteBtn, inputList);
+  editList(editBtn, inputList);
 });
+deleteList(deleteBtn, inputList);
+editList(editBtn, inputList);
