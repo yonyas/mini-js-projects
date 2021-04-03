0c013196cd54a2428e0a24d27ed2f71462847a21
Jiyon Lee <jiyonlee.d@gmail.com>
2021-04-03 23:00:03 +0900

    jumpgame hosting

--- gostop/script.js
+++
@@ -1,7 +0,0 @@
-let now = Date.now();
-if (!Date.now) {
-    Date.now = function{return new Date().getTime();}
-}
-
-const current = document.querySelector(".current");
-current.innerHTML = now;
--- 두더지게임/script.js
+++
@@ -1,9 +0,0 @@
-let tr = document.createElement("tr");
-for (i = 0; i < 3; i++) {
-  const td = document.createElement("td");
-  td.textContent = "hi";
-  tr.append(td);
-}
-const body = document.querySelector("body");
-console.dir(tr);
-body.append(tr);
